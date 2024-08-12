891a921a3acf5141e125cb9914a7703ba9fedc34
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-08-12 16:19:09 -0300

    Refatorando a Aplicação HTML e CSS em React com Typescript para estudar novos conceitos

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import Rotas from './routes';
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Rotas/>
+  </BrowserRouter>
+  )
+}
+export default App;
